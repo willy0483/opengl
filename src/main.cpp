6334c283d9f3c676ffbd281c1029d30b9c90efef
willy0483 <willyjensen251@gmail.com>
2025-12-04 18:59:08 +0100

    fix space

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ int main()
 	Shader shader("src/Shaders/vertex.vs", "src/Shaders/fragment.fs");
 
 	float vertices[] = {
-		// positions          // colors           // texture coords
+		// positions        // colors       // texture coords
 		0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
 		0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
